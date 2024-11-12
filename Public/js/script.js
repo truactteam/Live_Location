5ab6802a871143c1bfe84de398c3d4fa134a38a6
truactteam <truact.act@gmail.com>
2024-11-12 10:05:54 +0530

    first commit

---
+++ Public/js/script.js
@@ -0,0 +1,46 @@
+const socket = io();
+
+console.log("Hey");
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+        const{latitude, longitude} = position.coords;
+        socket.emit("send-location", {latitude, longitude});
+    },
+    (error)=>{
+        console.error(error);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    }
+);
+}
+
+const map = L.map("map").setView([0,0], 16);
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+    {
+        attribution: "OpenStreetMap"
+    }).addTo(map);
+
+const markers = {};
+
+socket.on("receive-location", (data)=>{
+    const{id, latitude, longitude} = data;
+    map.setView([latitude, longitude], 16);
+    if(markers[id]){
+        markers[id].setLatLng([latitude, longitude]);
+    }else{
+        markers[id] = L.marker([latitude, longitude]).addTo(map);
+    }
+});
+
+socket.on("user-disconnect", (id)=>{
+    if(markers[id]){
+        map.removeLayer(markers[id]);
+        delete markers[id];
+    }
+});
+
+console.log(markers);
---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+
+const http = require('http');
+
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on("connection", (socket)=>{
+    console.log("Socket Connected", socket.id);
+
+    socket.on("send-location", function(data){
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+    socket.on("disconnect", function(){
+        io.emit("user-disconnect", socket.id)
+    });
+})
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get('/',(req, res)=>{
+    res.render("index");
+})
+
+
+server.listen(3000);
